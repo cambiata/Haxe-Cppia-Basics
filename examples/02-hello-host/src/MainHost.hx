54355d49ebc24b59544d88c14a31504382b6af6c
Jonas Nystrom <jonasnys@gmail.com>
2016-10-11 13:57:20 +0200

    removed false comment

--- examples/02-hello-host/src/MainHost.hx
+++ examples/02-hello-host/src/MainHost.hx
@@ -2,6 +2,6 @@ class MainHost {
     static public function main() {
         trace('Hello from cppia HOST');
         var scriptname = './script.cppia';             
-        cpp.cppia.Host.runFile(scriptname); // <- doesn't work         
+        cpp.cppia.Host.runFile(scriptname);     
     }
 }
